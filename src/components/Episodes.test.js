6570fe739711fae4bc539a0d21cf159a8d28054b
SMoore3773 <Smooresculpture@gmail.com>
2020-04-25 12:52:35 -0400

    tests expanded for episodes

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import {render} from '@testing-library/react'
 import Episodes from './Episodes';
 
-test('episode renders', async()=>{
-    const mockEps=[];
-    render(<Episodes episodes={mockEps}/>)
+test('episode component renders', async()=>{
+    const mockEps=[{season:'1', number:'1'}];
+    const mockEps2=[{season:'1', number:'1'},{season:'1', number:'2'},{season:'1', number:'3'}]
+    const {queryAllByText, rerender} = render(<Episodes episodes={mockEps}/>)
+
+    let allEps = queryAllByText(/season/i)
+    expect(allEps).toHaveLength(1);
+    
+    rerender(<Episodes episodes={mockEps2}/>)
+    allEps = queryAllByText(/season/i);
+    expect(allEps).toHaveLength(1)
 })
